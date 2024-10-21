340af4dbc247ef91b1a06deb62afc2bc1996c211
kashmir <kuznetcov.alekcey@icloud.com>
2024-10-21 17:24:15 +0300

    Added the display of a list with posts using a class instead of a function.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,16 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
+
+
+class PostListView(ListView):
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 1
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
@@ -19,19 +29,19 @@ def post_detail(request, year, month, day, post):
     )
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 1)
-    page_number = request.GET.get("page", 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(
-        request,
-        "blog/post/list.html",
-        {"posts": posts}
-    )
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 1)
+#     page_number = request.GET.get("page", 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(
+#         request,
+#         "blog/post/list.html",
+#         {"posts": posts}
+#     )
