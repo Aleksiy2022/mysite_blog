0e9946e30c7df666025dcc5e7cdf75361a97b82d
kashmir <kuznetcov.alekcey@icloud.com>
2024-10-24 17:03:37 +0300

    Created view to sent email to share post.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from decouple import config
 from .models import Post
@@ -95,3 +96,28 @@ def post_detail(request, year, month, day, post):
 #         "blog/post/list.html",
 #         {"posts": posts}
 #     )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        post_id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {
+            "post": post,
+            "form": form,
+            "comment": comment
+         }
+    )
